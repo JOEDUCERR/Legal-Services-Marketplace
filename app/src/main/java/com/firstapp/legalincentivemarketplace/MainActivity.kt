dc70c626ad03b97a0288cf094ee3b62c5169bebb
Ratna Srivastava <srivastavaratna1611@gmail.com>
2025-04-15 16:36:44 +0530

    added files

--- app/src/main/java/com/firstapp/legalincentivemarketplace/MainActivity.kt
+++ app/src/main/java/com/firstapp/legalincentivemarketplace/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.firstapp.legalincentivemarketplace
 
+
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        bottomNav = findViewById(R.id.bottomNav)
+        setupBottomNavigation()
+        if (savedInstanceState == null) {
+            loadFragment(Dashboard.newInstance())
+        }
+    }
 
+    private fun setupBottomNavigation() {
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_dashboard -> loadFragment(Dashboard.newInstance())
+                R.id.nav_services -> loadFragment(Services.newInstance())
+                R.id.nav_profile -> loadFragment(Profile.newInstance())
+                else -> false
+            }
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment): Boolean {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+        return true
     }
 }
